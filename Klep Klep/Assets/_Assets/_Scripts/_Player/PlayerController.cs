6778aa4c5f912ea2e0c17b0a15e7a763e188acff
IdigArtGames <132227781+IdigArtGames@users.noreply.github.com>
2024-07-21 22:53:51 +0200

    Character Controller - Clap Clap

---
+++ Klep Klep/Assets/_Assets/_Scripts/_Player/PlayerController.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Locomotion
+    private CharacterController _controller;
+    private Vector3 _currentVelocity;
+    private const float walkSpeed = 1f;
+    private const float runSpeed = 3f;
+
+    //Animation vars
+    private bool _hasAnimator;
+    private Animator _anim;
+    private int _xVel;
+    private int _yVel;
+    private float _animLerpSpeed = 9f;
+
+    //look around u 
+    private float xRotation;
+    [SerializeField, Range (0, 200)] private float mouseSensitivity;
+    private Transform cam;
+    private float upperLimit = -75;
+    private float lowerLimit = 60f;
+
+    void Start()
+    {
+        _controller = GetComponent<CharacterController>();
+        cam = Camera.main.transform;
+
+        _hasAnimator = TryGetComponent (out _anim);
+
+        //get values from the _mainChar animator controller (found on the Animator Window)
+        _xVel = Animator.StringToHash("x_Velocity");
+        _yVel = Animator.StringToHash("y_Velocity");
+
+        //For Temp
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    void Update()
+    {
+        LookAround();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        if (!_hasAnimator) return;
+
+        var H = Input.GetAxis("Horizontal");
+        var V = Input.GetAxis("Vertical");
+
+        float targetSpeed = Input.GetKey(KeyCode.LeftShift) ? runSpeed : walkSpeed;
+       if (H == 0 && V == 0) targetSpeed = 0f;
+
+        _currentVelocity.x = Mathf.Lerp(_currentVelocity.x, H * targetSpeed, Time.deltaTime * _animLerpSpeed);
+        _currentVelocity.z = Mathf.Lerp(_currentVelocity.z, V * targetSpeed, Time.deltaTime * _animLerpSpeed);
+
+        _controller.Move(transform.TransformDirection(_currentVelocity) * Time.deltaTime);
+
+        _anim.SetFloat(_xVel, _currentVelocity.x);
+        _anim.SetFloat(_yVel, _currentVelocity.z);
+    }
+
+
+    private void LookAround()
+    {
+        var mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.smoothDeltaTime;
+        var mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.smoothDeltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, upperLimit, lowerLimit);
+
+        cam.localRotation = Quaternion.Euler(xRotation, 0, 0);
+        transform.Rotate(Vector3.up, mouseX);
+    }
+}
